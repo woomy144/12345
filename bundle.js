5d404d087f1741f9d42668f1f02f4104a809f5aa
woomy144 <73113918+woomy144@users.noreply.github.com>
2020-11-02 19:37:00 +0600

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -3714,7 +3714,7 @@
                     );
                   } else
                     W[6].draw(
-                      "Arras Template",
+                      "Forge Arras",
                       h + 200,
                       m - 42 - 2,
                       15,
