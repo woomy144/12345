f8fa41764a9b313c1eab9effa529a9ad73d77970
woomy144 <73113918+woomy144@users.noreply.github.com>
2021-02-15 18:37:28 +0600

    Update and rename Main.TS to Main.php

---
+++ TypeScript/Main.php
@@ -0,0 +1,43 @@
+<?php
+         echo "Hi";
+      ?>
+        
+<?php
+         echo "Jack";
+      ?>
+        
+        <?php
+         echo "Hellcat";
+      ?>
+        
+        <?php
+         echo "Fillygrove";
+      ?>
+        
+        <?php
+         echo "Lordtester2000";
+      ?>
+        
+        <?php
+         echo "Deram";
+      ?>
+        
+        <?php
+         echo "W8964";
+      ?>
+        
+        <?php
+         echo "Ghastboo";
+      ?>
+        
+        <?php
+         echo "King of mopeio";
+      ?>
+        
+        <?php
+         echo "Angry too";
+      ?>
+        
+        <?php
+         echo "Friends";
+      ?>
