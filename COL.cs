9cae17c175052aca09668223e6e3a4e7f58866fe
woomy144 <73113918+woomy144@users.noreply.github.com>
2021-04-16 22:39:45 +0600

    Update COL.cs

--- COL.cs
+++ COL.cs
@@ -1,13 +1,31 @@
+// assembly: System.Drawing.dll
+// assembly: System.Windows.Forms.dll
 using System;
+using System.Drawing;
+using System.Windows.Forms;
 
-namespace Example
+namespace WindowsForms
 {
-    class Program
+    public class Program
     {
-        static void Main()
+        [STAThread]
+        public static void Main()
         {
-            Console.WriteLine("Hello World!"); // Вывод заданного текста в консоль
-            Console.ReadKey(); // Ожидание нажатия клавиши пользователем
+            new DemoForm().ShowDialog();
+        }
+    }
+
+    public class DemoForm : Form
+    {
+        Label label = new Label();
+
+        public DemoForm()
+        {
+            label.Text = "Hello World!";
+            this.Controls.Add(label);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Color.White;
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
         }
     }
 }
