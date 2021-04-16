e1a5f7ad7139d4ea73cb27de028ba2d9951248bc
woomy144 <73113918+woomy144@users.noreply.github.com>
2021-04-16 22:37:24 +0600

    Create COL.cs

---
+++ COL.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!"); // Вывод заданного текста в консоль
+            Console.ReadKey(R); // Ожидание нажатия клавиши пользователем
+        }
+    }
+}
