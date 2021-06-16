360bc237aaa5a733082fa01d355a643ad13b9eef
woomy144 <73113918+woomy144@users.noreply.github.com>
2021-06-16 09:43:30 +0600

    Update app.js

--- js/js/js/js/secure/app.js
+++ js/js/js/js/secure/app.js
@@ -249,41 +249,6 @@ V¬°);h.±뀶ڢn램"²ܥx",¶¬._¢b¢{.m¨­Jܥx);¶¬._½ª.m¨­À.v.�
       }
     };
 
-// ===========================================================================
-        // Spaceball Shooter bullet.
-        // ===========================================================================
-        if (instance.layer === 20)
-        {           
-            if (!instance.spaceBallSprite)
-            {
-                let animationInfo = spaceBallSpriteInfo;
-                let destWidth = scale * ratio * animationInfo.width;
-                let destHeight = scale * ratio * animationInfo.height;
-
-                instance.spaceBallSprite = new Sprite(animationInfo.url, 
-                                            [0, 0], // Position
-                                            [animationInfo.width, animationInfo.height], // Source size 
-                                            [destWidth, destHeight], // Destination size
-                                            24, // Animation speed
-                                            animationInfo.frames, // Frames
-                                            null, // Direction (default is horizontal)
-                                            false); // Play once                   
-            }
-           
-            var now = getNow();
-            var dt = instance.spaceBallTime ? (now - instance.spaceBallTime) / 1000.0 : 0;
-            
-            // Add 90 degrees to align with the rotation of the bullet.
-            let rotation = rot + 180 * (Math.PI / 180);
-            let alpha = instance.render.status.getFade();
-            
-            // dt, destX, destY, destXOffset, destYOffset,  rotation, alpha
-            instance.spaceBallSprite.update(dt, x, y, 0,0, rotation, alpha);
-            instance.spaceBallSprite.render(ctx);   
-
-            instance.spaceBallTime = now;                           
-        }
-        // ===========================================================================
 
     I.stringPadding = function(r, p) {
       r = void 0 !== r ? String(r) : " ";
