2cddec773ce8bd687d8fb623b9a2d8b7a0414448
woomy144 <73113918+woomy144@users.noreply.github.com>
2021-07-11 01:28:24 +0600

    Update app.js

--- js/js/js/js/secure/app.js
+++ js/js/js/js/secure/app.js
@@ -203,8 +203,7 @@ resources.load([
 let spaceBallSpriteInfo = {
         url: 'img/space_ball.png', 
         width: 93,
-        height: 100,        
-        frames: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+        height: 100
     };
 // ==============================================================================
 
@@ -227,42 +226,6 @@ let spaceBallSpriteInfo = {
       );
     return r + "";
   };
-
- // ===========================================================================
-        // Spaceball Shooter bullet.
-        // ===========================================================================
-        if (instance.layer === 20)
-        {           
-            if (!instance.spaceBallSprite)
-            {
-                let animationInfo = spaceBallSpriteInfo;
-                let destWidth = scale * ratio * animationInfo.width;
-                let destHeight = scale * ratio * animationInfo.height;
-
-                instance.spaceBallSprite = new Sprite(animationInfo.url, 
-                                            [0, 0], // Position
-                                            [animationInfo.width, animationInfo.height], // Source size 
-                                            [destWidth, destHeight], // Destination size
-                                            24, // Animation speed
-                                            animationInfo.frames, // Frames
-                                            null, // Direction (default is horizontal)
-                                            false); // Play once                   
-            }
-           
-            var now = getNow();
-            var dt = instance.spaceBallTime ? (now - instance.spaceBallTime) / 1000.0 : 0;
-            
-            // Add 90 degrees to align with the rotation of the bullet.
-            let rotation = rot + 180 * (Math.PI / 180);
-            let alpha = instance.render.status.getFade();
-            
-            // dt, destX, destY, destXOffset, destYOffset,  rotation, alpha
-            instance.spaceBallSprite.update(dt, x, y, 0,0, rotation, alpha);
-            instance.spaceBallSprite.render(ctx);   
-
-            instance.spaceBallTime = now;                           
-        }
-        // ===========================================================================
 
   I.ASSUME_ES5 = !1;
   I.ASSUME_NO_NATIVE_MAP = !1;
