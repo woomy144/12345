cc3db7b77cface549d8aa6e79f89a91e3ad1d83d
woomy144 <73113918+woomy144@users.noreply.github.com>
2021-07-14 10:08:42 +0600

    Update app.js

--- js/js/js/js/secure/app.js
+++ js/js/js/js/secure/app.js
@@ -1566,7 +1566,7 @@ V��);h.�??n?"??x",��._??b?{.m?�J?x);��._??.m?�A.v?.??n?r.j?^
 				y[0].style.opacity = b ? 0 : 1;
 				y[2].style.opacity = b ? 1 : 0;
 			  };
-               m = () => {
+               g = () => {
 				v.style.transform = v.style.webkitTransform = b
 				  ? "translate(2px, -2px) rotate(45deg)"
 				  : "rotate(-45deg)";
@@ -1577,7 +1577,7 @@ V��);h.�??n?"??x",��._??b?{.m?�J?x);��._??.m?�A.v?.??n?r.j?^
 			  };
 			j.onclick = () => {
 			  b = !b;
-			  m();
+			  g();
 			};
 				a.onclick = () => {
 			  b = !b;
