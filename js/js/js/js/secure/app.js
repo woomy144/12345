49b9bcf3224b2a136be5bb44bdd02d7f5564df64
Iliya-Kiporiev <73113918+woomy144@users.noreply.github.com>
2021-10-15 23:46:03 +0600

    Update app.js

--- js/js/js/js/secure/app.js
+++ js/js/js/js/secure/app.js
@@ -4095,6 +4095,14 @@ break;
                   );
                 else {
                   if (b.showDebug) {
+                     W[7].draw(
+                        "Connect Status" + setInterval(() =>{Math.random, Math.floor},300),
+                       h + 200,
+                       m - 84 - 2,
+                       15,
+                       l.white,
+                       "right"
+                     );
                     W[6].draw(
                        "Tek-arras.cc",
                       h + 200,
