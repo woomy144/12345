f743b9f2346d8ef6f6fa28e3af050aac12bdf0f8
Iliya-Kiporiev <73113918+woomy144@users.noreply.github.com>
2021-10-18 01:53:30 +0600

    Update app.js

--- js/js/js/js/secure/app.js
+++ js/js/js/js/secure/app.js
@@ -4449,8 +4449,8 @@ break;
              
                 g.fillStyle = l.black,
                 g.globalAlpha = 0.7,
-                V.draw("id: ", l.guiwhite, "center"),
-              G(1220, 538, 200, 30, 0);
+              G(1220, 538, 200, 30, 0),
+              V("id: ", l.guiwhite, "center");
               //1 = left, 2=top 3=widht, 4=height, 5=?
               if (b.mobile) {
                 b.canSkill =
