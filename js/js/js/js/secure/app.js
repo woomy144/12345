92010c0b603e228be1dcee75456f69831ec4626c
Iliya-Kiporiev <73113918+woomy144@users.noreply.github.com>
2021-10-18 01:04:45 +0600

    Update app.js

--- js/js/js/js/secure/app.js
+++ js/js/js/js/secure/app.js
@@ -4448,8 +4448,8 @@ break;
               }*/
              
                 g.fillStyle = l.black,
-                g.globalAlpha = 1,
-              G(700, 600, 180, 30, 0);
+                g.globalAlpha = 0.7,
+              G(700, 550, 180, 30, 0);
               //1 = top, 2=left 3=widht, 4=height, 5=?
               if (b.mobile) {
                 b.canSkill =
