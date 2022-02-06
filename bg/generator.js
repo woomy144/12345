dc0dfbcd3654ffe48dc71bf448de9a3aff6aaf92
Iliya-Kiporiev <73113918+woomy144@users.noreply.github.com>
2022-02-06 10:55:02 +0600

    Delete bg directory

--- bg/generator.js
+++
@@ -1,1 +0,0 @@
-document.getElementById('bgran').style.backgroundImage = "<img src='./pentagon.png' style='top:%; left:%; position:absolute'>".replace(/%/g, function() { return Math.random() * 1000 | 0 } );
